1d4099039e3ae28b73ad891be885f1d29559e1f8
veronicamanukyan <vermanuk@mail.ru>
2022-03-16 16:39:37 +0300

    Add

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,11 @@
 """
 Scrapper implementation
 """
+import json
+import os
+import re
+
+import requests
 
 
 class IncorrectURLError(Exception):
@@ -25,8 +30,11 @@ class Crawler:
     """
     Crawler implementation
     """
+
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.max_articles = max_articles
+        self.seed_urls = seed_urls
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -35,7 +43,7 @@ class Crawler:
         """
         Finds articles
         """
-        pass
+
 
     def get_search_urls(self):
         """
@@ -48,14 +56,30 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        pass
+    finally:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path) as file:
+        config = json.load(file)
+    max_articles = config["total_articles_to_find_and_parse"]
+    seed_urls = config["seed_urls"]
+    if not isinstance(max_articles, int):
+        raise IncorrectNumberOfArticlesError
+    if max_articles > 200:
+        raise NumberOfArticlesOutOfRangeError
+    part_of_string = re.compile(r'^https?://')
+    for url in seed_urls:
+        if not part_of_string.search(url):
+            raise IncorrectURLError
 
 
 if __name__ == '__main__':
