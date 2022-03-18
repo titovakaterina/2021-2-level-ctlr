772b58f739caf8a092ca6024d7b99d762a032fa7
veronicamanukyan <vermanuk@mail.ru>
2022-03-18 17:57:06 +0300

    'four'

--- scrapper.py
+++ scrapper.py
@@ -4,8 +4,11 @@ Scrapper implementation
 import json
 import os
 import re
-
 import requests
+from bs4 import BeautifulSoup
+
+from constants import ASSETS_PATH
+from core_utils.article import Article
 
 
 class IncorrectURLError(Exception):
@@ -37,13 +40,30 @@ class Crawler:
         self.urls = []
 
     def _extract_url(self, article_bs):
-        pass
+        urls_bs = article_bs.find_all('a', class_='nohover')
+        beginning_of_link = 'https://elementy.ru/'
+        urls_bs_full = []
+        for url_bs in urls_bs:
+            not_full_link = url_bs['href']
+            urls_bs_full.append(f'{beginning_of_link}{not_full_link}')
+
+        return urls_bs_full
 
     def find_articles(self):
         """
         Finds articles
         """
+        for seed_url in self.seed_urls:
+            response = requests.get(url=seed_url)
 
+            if not response.ok:
+                continue
+
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            articles_urls = self._extract_url(soup)
+            for full_url in articles_urls:
+                self.urls.append(full_url)
 
     def get_search_urls(self):
         """
@@ -52,16 +72,39 @@ class Crawler:
         pass
 
 
+class HTMLParser:
+    def __init__(self, article_url, article_id):
+        self.article_url = article_url
+        self.article_id = article_id
+        self.article = Article(article_url, article_id)
+
+    def _fill_article_with_text(self, article_bs):
+        divs = article_bs.find('div', class_='memo')
+        ps = divs.find_all('p')
+        for p in ps:
+            self.article.text += p.text.strip()
+
+    def parse(self):
+        response = requests.get(url=self.article_url)
+
+        article_bs = BeautifulSoup(response.text, 'lxml')
+
+        self._fill_article_with_text(article_bs)
+        self.article.save_raw()
+
+        return self.article
+
+
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
     try:
-        os.rmdir(base_path)
+        os.removedirs(base_path)
     except FileNotFoundError:
         pass
     finally:
-        os.mkdir(base_path)
+        os.makedirs(base_path)
 
 
 def validate_config(crawler_path):
@@ -72,16 +115,22 @@ def validate_config(crawler_path):
         config = json.load(file)
     max_articles = config["total_articles_to_find_and_parse"]
     seed_urls = config["seed_urls"]
-    if not isinstance(max_articles, int):
+    if not seed_urls:
+        raise IncorrectURLError
+
+    if not isinstance(max_articles, int) or max_articles <= 0:
         raise IncorrectNumberOfArticlesError
+
     if max_articles > 200:
         raise NumberOfArticlesOutOfRangeError
+
     part_of_string = re.compile(r'^https?://')
+
     for url in seed_urls:
         if not part_of_string.search(url):
             raise IncorrectURLError
+    return seed_urls, max_articles
 
 
 if __name__ == '__main__':
-    # YOUR CODE HERE
-    pass
+    prepare_environment(ASSETS_PATH)
