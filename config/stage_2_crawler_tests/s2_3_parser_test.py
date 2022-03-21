54928b14b21bf1f053745b84a70f740af97a934d
Alexander Demidovskij <monadv@yandex.ru>
2022-03-21 10:01:37 +0300

    [CI] Closing a gap for PDF Parser (#61)

--- config/stage_2_crawler_tests/s2_3_parser_test.py
+++ config/stage_2_crawler_tests/s2_3_parser_test.py
@@ -7,8 +7,7 @@ import pytest
 
 from core_utils.article import Article
 from scrapper import validate_config, Crawler, HTMLParser
-from constants import CRAWLER_CONFIG_PATH
-from urllib.parse import urljoin
+from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
 
 
 class HTMLParserTest(unittest.TestCase):
@@ -18,11 +17,19 @@ class HTMLParserTest(unittest.TestCase):
         with open(CRAWLER_CONFIG_PATH, encoding='utf-8') as file:
             data = json.load(file)
 
+        if not ASSETS_PATH.exists():
+            ASSETS_PATH.mkdir(parents=True)
+
         self.crawler = Crawler(data['seed_urls'],
                                data['total_articles_to_find_and_parse'])
         self.crawler.find_articles()
         self.parser = HTMLParser(random.choice(self.crawler.urls), 1)
         self.return_value = self.parser.parse()
+
+    def tearDown(self) -> None:
+        for pdf_file in ASSETS_PATH.glob('*.pdf'):
+            print(f'Removing {pdf_file}')
+            pdf_file.unlink()
 
     @pytest.mark.mark4
     @pytest.mark.mark6
