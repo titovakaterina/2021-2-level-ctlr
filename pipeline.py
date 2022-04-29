76555e8cf100d906b7832961976dd08af0be95f7
titovakaterina <titova.katerina2017@yandex.ru>
2022-04-29 03:45:32 +0300

    hi

--- pipeline.py
+++ pipeline.py
@@ -2,10 +2,19 @@
 Pipeline for text processing implementation
 """
 
+from pathlib import Path
+import re
+
+import pymorphy2
+from pymystem3 import Mystem
+
+from constants import ASSETS_PATH
+from core_utils.article import Article, ArtifactType
+
 
 class EmptyDirectoryError(Exception):
     """
-    No_data to process
+    No data to process
     """
 
 
@@ -24,25 +33,29 @@ class MorphologicalToken:
     """
 
     def __init__(self, original_word):
+        self.original_word = original_word
+        self.normalized_form = ''
+        self.tags_mystem = ''
+        self.tags_pymorphy = ''
         pass
 
     def get_cleaned(self):
         """
         Returns lowercased original form of a token
         """
-        pass
+        return self.original_word.lower()
 
     def get_single_tagged(self):
         """
         Returns normalized lemma with MyStem tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>'
 
     def get_multiple_tagged(self):
         """
         Returns normalized lemma with PyMorphy tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>({self.tags_pymorphy})'
 
 
 class CorpusManager:
@@ -51,19 +64,28 @@ class CorpusManager:
     """
 
     def __init__(self, path_to_raw_txt_data: str):
+        self.path_to_raw_txt_data = Path(path_to_raw_txt_data)
+        self._storage = {}
+        self._scan_dataset()
         pass
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        path_to_raw = Path(self.path_to_raw_txt_data)
+
+        for file in path_to_raw.glob('*_raw.txt'):
+            article_id = int(file.stem.split('_')[0])
+            self._storage[article_id] = Article(url=None, article_id=article_id)
+
+    pass
 
     def get_articles(self):
         """
         Returns storage params
         """
-        pass
+        return self._storage
 
 
 class TextProcessingPipeline:
@@ -72,109 +94,112 @@ class TextProcessingPipeline:
     """
 
     def __init__(self, corpus_manager: CorpusManager):
+        self.corpus_manager = corpus_manager
         pass
 
     def run(self):
         """
         Runs pipeline process scenario
         """
+        articles = self.corpus_manager.get_articles().values()
+
+        for article in articles:
+            raw_text = article.get_raw_text()
+            tokens = self._process(raw_text)
+
+            clean = []
+            single_tagged = []
+            multiple_tagged = []
+
+            for token in tokens:
+                clean.append(token.get_cleaned())
+                single_tagged.append(token.get_single_tagged())
+                multiple_tagged.append(token.get_multiple_tagged())
+
+            article.save_as(' '.join(clean), ArtifactType.cleaned)
+            article.save_as(' '.join(single_tagged), ArtifactType.single_tagged)
+            article.save_as(' '.join(multiple_tagged), ArtifactType.multiple_tagged)
         pass
 
     def _process(self, raw_text: str):
         """
         Processes each token and creates MorphToken class instance
         """
-        pass
+        pattern = re.compile(r'[а-яА-Яa-zA-z ёЁ]')
+        cleaned_text = raw_text
+        for letter in raw_text:
+            if not pattern.match(letter):
+                cleaned_text = raw_text.replace(letter, ' ')
+
+        morph_analyzer = pymorphy2.MorphAnalyzer()
+        analyzed_text = Mystem().analyze(cleaned_text)
+        tokens = []
+
+        for single_word_analysis in analyzed_text:
+            if 'analysis' not in single_word_analysis:
+                continue
+            if not single_word_analysis['analysis']:
+                continue
+
+            token = MorphologicalToken(single_word_analysis['text'])
+            tokens.append(token)
+            token.normalized_form = single_word_analysis['analysis'][0]['lex']
+            token.tags_mystem = single_word_analysis['analysis'][0]['gr']
+            token.tags_pymorphy = morph_analyzer.parse(single_word_analysis['text'])[0].tag
+
+        return tokens
 
 
 def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
+    if isinstance(path_to_validate, str):
+        path_to_validate = Path(path_to_validate)
+
+    if not path_to_validate.exists():
+        raise FileNotFoundError
+
+    if not path_to_validate.is_dir():
+        raise NotADirectoryError
+
+    if not any(path_to_validate.iterdir()):
+        raise EmptyDirectoryError
+
+    files = list(path_to_validate.glob('*'))
+    if not files:
+        raise EmptyDirectoryError
+
+    json_counter = 0
+    raw_counter = 0
+
+    for file in sorted(path_to_validate.glob('*'), key=lambda x: int(x.name[:x.name.find('_')])):
+        if file.name.endswith('raw.txt'):
+            raw_counter += 1
+
+            if f'{raw_counter}_raw' not in file.name:
+                raise InconsistentDatasetError
+
+            with open(file, 'r', encoding='utf-8') as this_file:
+                text = this_file.read()
+            if not text:
+                raise InconsistentDatasetError
+
+        if file.name.endswith('meta.json'):
+            json_counter += 1
+
+    if json_counter != raw_counter:
+        raise InconsistentDatasetError
     pass
 
 
-from pymystem3 import Mystem
-from pathlib import Path
-
 def main():
-
-    mystem = Mystem()
-    file_path = Path(__file__).parent / 'seminars' / '04.15.2022' / 'test.txt'
-    with file_path.open(encoding='utf-8') as file:
-        plain_text = file.read()
-
-    tokens = mystem.lemmatize(plain_text)
-    clear = []
-    pm = ['.', ',', '/', '!']
-    for i in tokens:
-        str_i = i.strip()
-        if str_i in pm:
-            if '' in i:
-                clear.append(i)
-            continue
-        else:
-            clear.append(i)
-
-    for i in clear:
-        ''.join(tokens)
-    print(''.join(tokens))
-
-    plain_text_analysis = mystem.analize(plain_text)\
-    print(plain_text_analysis)
-
-    count = 0
-
-    for i in plain_text:
-        if not i.get('analysis'):
-            continue
-        gr = i.get('analysis')[0].get('gr')
-        if 'S,' in gr:
-            print(gr)
-            count+=1
-    print(count)
+    # YOUR CODE HERE
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
-        print ('word: ', i, gr)
-
-import re
-
-def mmain():
-    name = 'Tom Gayler, 19 y.o.'
-    p = re.compile(r'^[A-Z][a-z] \w+')
-    p = re.compile(r'\w+ \w+, \d{1,3} y\.o\.')
-
-    res=p.match(name)
-
-    if res:
-        print('String was matched')
-
-    print(res.span())
-    print(res.group())
-
-    p=re.compile(r'\w+ \w+, \d{1,3} y\.o\.')
-    res=p.match(name)
-
-    print(res.group(0))
-    print(res.group(1))
-    print(res.group(2))
-    print(res.group(3))
-
-    file_path = Path(__file__).parent / 'seminars' / '04.22.2002' / 'logdata.txt'
-    with open (file_path) as file:
-        text = file.readlines()
-    string = '146.204.224.152'
-    pattern = re.compile(r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}')
-    res = pattern.findall(text)
-    print(len(res))
-
-    with open(file_path) as file:
-        text = file.readlines()
-    headers_p = re.compile(r'POST|GET|PUT|DELETE')
-    res = headers_p.findall(text)
-    print(len(res))
-
-
-
-    if __name__=='__main__':
-        main()
+if __name__ == "__main__":
+    main()
